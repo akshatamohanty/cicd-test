3839d7cd6ff61db3f408ddc5170505e2c5dca8c2
Akshata Mohanty <akshatamohanty@gmail.com>
2020-11-21 13:28:16 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.json('Hello Techladies!')
+  res.json('Hello amazing Techladies!')
 })
 
 module.exports = app.listen(port)
